3bc6cc7e0d44ecbf70c9459644624aaa30b261aa
Shreya0207 <shanu.shreya270@gmail.com>
2020-10-27 20:03:30 +0530

    [Shreya]Add: Removing the word from large paragraph

--- HashTableProgram/MyMapNode.cs
+++ HashTableProgram/MyMapNode.cs
@@ -104,6 +104,24 @@ namespace HashTableProgram
                 }
             }
         }
+        public void RemoveValue(V value)
+        {
+            foreach (LinkedList<KeyValue<K, V>> list in items)
+            {
+                if (list == null)
+                    continue;
+                foreach (KeyValue<K, V> obj in list)
+                {
+                    if (obj.Equals(null))
+                        continue;
+                    if (obj.Value.Equals(value))
+                    {
+                        Remove(obj.Key);
+                        break;
+                    }
+                }
+            }
+        }
     }
     public class KeyValue<K, V>
     {
--- HashTableProgram/Program.cs
+++ HashTableProgram/Program.cs
@@ -27,8 +27,9 @@ namespace HashTableProgram
             hash.Add("16", "paranoid");
             hash.Add("17", "avoidable");
             hash.Add("18", "situations");
-            hash.GetFrequency("paranoid");
-            hash.DisplayFrequency();
+            hash.GetFrequency("avoidable");
+            hash.RemoveValue("avoidable");
+            hash.GetFrequency("avoidable");
         }
     }
 }
