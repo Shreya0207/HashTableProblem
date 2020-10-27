0e3e700c41430367d73b9d13399b3140d157eb01
Shreya0207 <shanu.shreya270@gmail.com>
2020-10-27 19:52:21 +0530

    [Shreya]Add: Finding the frequency

--- HashTableProgram/Program.cs
+++ HashTableProgram/Program.cs
@@ -7,6 +7,14 @@ namespace HashTableProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hash Table Program");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+            hash.GetFrequency("or");
         }
     }
 }
